b7225229456f51894ed743d0e9f2147ca0e13f34
bluezooo <2570633265@qq.com>
2023-11-14 09:16:06 -0500

    quicksort mpi with slow k-way merge

--- project3/src/quicksort/mpi.cpp
+++ project3/src/quicksort/mpi.cpp
@@ -10,8 +10,10 @@
 #include <vector>
 #include <mpi.h>
 #include "../utils.hpp"
+#include <limits>
 
 #define MASTER 0
+#define TAG_GATHER 0
 
 int partition(std::vector<int> &vec, int low, int high) {
     int pivot = vec[high];
@@ -28,11 +30,65 @@ int partition(std::vector<int> &vec, int low, int high) {
     return i + 1;
 }
 
-void quickSort(std::vector<int>& vec, int numtasks, int taskid, MPI_Status* status) {
+void quickSortHelper(std::vector<int> &vec, int low, int high) {
+    if (low < high) {
+        int pivotIndex = partition(vec, low, high);
+        quickSortHelper(vec, low, pivotIndex - 1);
+        quickSortHelper(vec, pivotIndex + 1, high);
+    }
+}
+
+void quickSort(std::vector<int>& vec, int numtasks, int taskid, MPI_Status* status, int size) {
     /* Your code here!
        Implement parallel quick sort with MPI
     */
+    int i = 0;
+    // std::assert(taskid ==0);
+    int length_per_task = (size + numtasks - 1 )/numtasks;
+    std::vector<int> cuts(numtasks + 1, 0);
+    while (i < numtasks-1){
+        cuts[i+1] = cuts[i]+length_per_task;
+        i++;
+    }
+    cuts[numtasks] = size;
+    std::vector<int> index (numtasks, 0);
+    std::vector<int> length (numtasks, length_per_task);
+    length[numtasks-1] = cuts[numtasks] - cuts[numtasks-1];
+
+    quickSortHelper(vec, cuts[taskid], cuts[taskid+1]-1);
+    
+    if (taskid == MASTER) {
+        for (int i = MASTER + 1; i < numtasks; i++) {
+            int* start_pos = &vec[cuts[i]];
+            MPI_Recv(start_pos, length[i], MPI_INT, i, TAG_GATHER, MPI_COMM_WORLD, status);
+        }
+        //merge the k sorted arrays, each from cuts[i] to cuts[i+1] -1
+        std::vector<int> vec_clone = vec;
+        std::vector <int>::iterator it = vec.begin();
+        while (it != vec.end()){
+            int min = std::numeric_limits<int>::max();
+            int arg_min;
+            for (int j = 0; j < numtasks; j++){
+                if (index[j] < length[j]){
+                    int current = vec_clone[cuts[j]+index[j]];
+                    if (current < min){
+                        min = current;
+                        arg_min = j;
+                    }
+                }
+            }
+            index[arg_min]++;
+            * it = min;
+            it++;
+        }
+    }
+    else{
+        int * ptr = &vec[cuts[taskid]];
+        MPI_Send(ptr , length[taskid] , MPI_INT , MASTER , TAG_GATHER , MPI_COMM_WORLD);
+    }
 }
+
+
 
 int main(int argc, char** argv) {
     // Verify input argument format
@@ -64,7 +120,7 @@ int main(int argc, char** argv) {
 
     auto start_time = std::chrono::high_resolution_clock::now();
 
-    quickSort(vec, numtasks, taskid, &status);
+    quickSort(vec, numtasks, taskid, &status, size);
 
     if (taskid == MASTER) {
         auto end_time = std::chrono::high_resolution_clock::now();
